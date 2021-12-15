30fa2fce7d24cc0b27dd06d9233dd9b5edc82b9e
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2021-12-15 10:41:43 +0100

    init

---
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+usermod -d /var/lib/mysql/ mysql
+service mysql start
+mysql_secure_installation << EOF
+
+Y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+Y
+Y
+Y
+Y
+EOF
+mysql -e "CREATE DATABASE wordpress charset utf8mb4 collate utf8mb4_unicode_ci"
+mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'*'"
+mysql -e "FLUSH PRIVILEGES"
+service mysql stop
+mysqld_safe
+#bash
