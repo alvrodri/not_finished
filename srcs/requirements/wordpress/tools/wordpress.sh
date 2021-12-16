b9b21b50381b0505c008b5506b9e9d6fac79426d
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2021-12-16 18:12:29 +0100

    a

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 tar xf /latest-es_ES.tar.gz -C /var/www/html
+mv /wp-config.php /var/www/html/wordpress/wp-config.php
 chown www-data:www-data /var/www/html/wordpress/ -R
 php-fpm7.3 -F
