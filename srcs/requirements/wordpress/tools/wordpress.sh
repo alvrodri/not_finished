43a86d81db20b0837e4a2efd9882ba822e210ba1
alvrodri <alvaromarianorodriguezbenitez@gmail.com>
2021-12-17 11:26:11 +0100

    fix wp?

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 tar xf /latest-es_ES.tar.gz -C /var/www/html
-mv /wp-config.php /var/www/html/wordpress/wp-config.php
+mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+sed -i "s/username_here/$MYSQL_USER/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/password_here/$MYSQL_PASSWORD/g" /var/www/html/wordpress/wp-config.php
+sed -i "s/localhost/$MYSQL_HOST/g" /var/www/html/wordpress/wp-config.php
 chown www-data:www-data /var/www/html/wordpress/ -R
 php-fpm7.3 -F
